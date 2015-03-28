248cd377bb3e8a89076cb457bfa3a23ea062cab8
Paul <soapmctravich@gmail.com>
2015-03-28 16:45:41 +0100

    Fin de page basique pour chaque personnage

--- app/src/main/java/net/fancysoftware/wiki_of_the_storm/personnages_Fragment.java
+++ app/src/main/java/net/fancysoftware/wiki_of_the_storm/personnages_Fragment.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 
 /**
  * Created by Mathieu on 09/03/2015.
@@ -27,6 +29,8 @@ public class personnages_Fragment extends android.support.v4.app.Fragment{
         String origine = Personnage.PersonnageStatic.personnage.getOrigine();
         String type = Personnage.PersonnageStatic.personnage.getType();
         String histoire = Personnage.PersonnageStatic.personnage.getHistoire();
+        ArrayList<Basique> basiques = Personnage.PersonnageStatic.personnage.getBasiques();
+        ArrayList<Heroique> heroiques = Personnage.PersonnageStatic.personnage.getHeroiques();
 
         TextView nomPerso = (TextView)rootview.findViewById(R.id.nom_perso);
         nomPerso.setText(nom);
@@ -35,10 +39,28 @@ public class personnages_Fragment extends android.support.v4.app.Fragment{
         originePerso.setText(getResources().getString(R.string.origine) + ": " + origine);
 
         TextView typePerso = (TextView)rootview.findViewById(R.id.type_perso);
-        typePerso.setText(type);
+        typePerso.setText(getResources().getString(R.string.type) + ": " + type);
 
         TextView histoirePerso = (TextView)rootview.findViewById(R.id.histoire_perso);
         histoirePerso.setText(histoire);
+
+        for (int i = 0; i < basiques.size(); i++){
+            LinearLayout linearLayoutBasqiue = (LinearLayout)rootview.findViewById(R.id.skillsbasiques);
+
+            TextView basiquePerso = new TextView(rootview.getContext());
+            basiquePerso.setText(basiques.get(i).getNom());
+
+            linearLayoutBasqiue.addView(basiquePerso);
+        }
+
+        for (int i = 0; i < heroiques.size(); i++){
+            LinearLayout linearLayoutHeroique = (LinearLayout)rootview.findViewById(R.id.skillsheroiques);
+
+            TextView heroiquePerso = new TextView(rootview.getContext());
+            heroiquePerso.setText(heroiques.get(i).getNom());
+
+            linearLayoutHeroique.addView(heroiquePerso);
+        }
 
         return rootview;
     }
