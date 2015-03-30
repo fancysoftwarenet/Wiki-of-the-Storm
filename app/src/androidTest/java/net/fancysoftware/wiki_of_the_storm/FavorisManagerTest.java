7b55e63b0c3ed2a744c7fba26ab83dbe20fa0b62
Paul <soapmctravich@gmail.com>
2015-03-30 21:08:40 +0200

    Ajout test favoris

---
+++ app/src/androidTest/java/net/fancysoftware/wiki_of_the_storm/FavorisManagerTest.java
@@ -0,0 +1,26 @@
+package net.fancysoftware.wiki_of_the_storm;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by Paul on 30/03/2015.
+ */
+public class FavorisManagerTest extends InstrumentationTestCase{
+    FavorisManager fm;
+    Personnage personnage;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        fm = new FavorisManager();
+        personnage = Personnage.PersonnageStatic.personnages.get(1);
+    }
+
+    public void testAddFavoris(){
+        fm.WriteFavoris(null, personnage);
+
+        assertTrue(fm.isFavoris(null, Integer.toString(personnage.getID())));
+        assertFalse(fm.isFavoris(null, "7"));
+    }
+}
